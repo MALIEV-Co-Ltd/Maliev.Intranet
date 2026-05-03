a1e38dd3068e83ce3089f3c233a64dd769b854a4
Natthapol Vanasrivilai <natthapol.vanasrivilai@maliev.com>
2026-05-03 23:04:49 +0700

    Keep global search decorations inside input

--- Maliev.Intranet.Tests/Client/Components/GlobalSearchBoxTests.cs
+++ Maliev.Intranet.Tests/Client/Components/GlobalSearchBoxTests.cs
@@ -12,6 +12,39 @@ namespace Maliev.Intranet.Tests.Client.Components;
 public class GlobalSearchBoxTests
 {
     [Fact]
+    public void GlobalSearchBox_RendersIconInsideInputWrapper()
+    {
+        using var context = CreateContext(_ => JsonContent.Create(new GlobalSearchResponseDto("acme", 0, [])));
+
+        var cut = context.Render<GlobalSearchBox>();
+
+        var icon = cut.Find(".global-search-input-wrap > .global-search-icon");
+        Assert.Equal("span", icon.LocalName);
+        Assert.NotNull(cut.Find(".global-search-input-wrap > input.global-search-input"));
+    }
+
+    [Fact]
+    public void GlobalSearchBox_RendersSpinnerInsideInputWrapperWhileLoading()
+    {
+        var response = new TaskCompletionSource<HttpResponseMessage>();
+        using var context = CreateContext(_ => response.Task);
+
+        var cut = context.Render<GlobalSearchBox>();
+        cut.Find("input").Input("acme");
+
+        cut.WaitForAssertion(() =>
+        {
+            var spinner = cut.Find(".global-search-input-wrap > .global-search-spinner");
+            Assert.Equal("span", spinner.LocalName);
+        });
+
+        response.SetResult(new HttpResponseMessage(HttpStatusCode.OK)
+        {
+            Content = JsonContent.Create(new GlobalSearchResponseDto("acme", 0, []))
+        });
+    }
+
+    [Fact]
     public async Task GlobalSearchBox_DoesNotSearchBelowTwoCharacters()
     {
         var calls = 0;
@@ -30,7 +63,7 @@ public class GlobalSearchBoxTests
     }
 
     [Fact]
-    public void GlobalSearchBox_RendersResultsAndNavigatesOnClick()
+    public async Task GlobalSearchBox_RendersResultsAndNavigatesOnClick()
     {
         var customerId = Guid.NewGuid();
         using var context = CreateContext(_ => JsonContent.Create(new GlobalSearchResponseDto(
@@ -51,14 +84,14 @@ public class GlobalSearchBoxTests
         cut.Find("input").Input("acme");
 
         cut.WaitForAssertion(() => Assert.Contains("Acme Corp", cut.Markup));
-        cut.Find(".global-search-result").Click();
+        await cut.InvokeAsync(() => cut.Find(".global-search-result").Click());
 
         var navigation = context.Services.GetRequiredService<NavigationManager>();
         Assert.EndsWith($"/customers/{customerId}", navigation.Uri, StringComparison.Ordinal);
     }
 
     [Fact]
-    public void GlobalSearchBox_EnterNavigatesFirstResult()
+    public async Task GlobalSearchBox_EnterNavigatesFirstResult()
     {
         var projectId = Guid.NewGuid();
         using var context = CreateContext(_ => JsonContent.Create(new GlobalSearchResponseDto(
@@ -80,7 +113,7 @@ public class GlobalSearchBoxTests
         input.Input("fixture");
 
         cut.WaitForAssertion(() => Assert.Contains("Fixture", cut.Markup));
-        input.KeyDown("Enter");
+        await cut.InvokeAsync(() => cut.Find("input").KeyDown("Enter"));
 
         var navigation = context.Services.GetRequiredService<NavigationManager>();
         Assert.EndsWith($"/sales/projects/{projectId}", navigation.Uri, StringComparison.Ordinal);
@@ -88,13 +121,18 @@ public class GlobalSearchBoxTests
 
     private static BunitContext CreateContext(Func<HttpRequestMessage, HttpContent> contentFactory)
     {
+        return CreateContext(request => Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK)
+        {
+            Content = contentFactory(request)
+        }));
+    }
+
+    private static BunitContext CreateContext(Func<HttpRequestMessage, Task<HttpResponseMessage>> responseFactory)
+    {
         var context = new BunitContext();
         context.Services.AddMudServices();
         context.Services.AddSingleton(new HttpClient(new MockHttpMessageHandler((request, _) =>
-            Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK)
-            {
-                Content = contentFactory(request)
-            })))
+            responseFactory(request)))
         {
             BaseAddress = new Uri("http://test/")
         });
