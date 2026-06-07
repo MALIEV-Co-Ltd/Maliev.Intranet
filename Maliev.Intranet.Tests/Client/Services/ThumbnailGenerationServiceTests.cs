6ab0bc12cb374a34bd818dd910102a32e944219a
Natthapol Vanasrivilai <natthapol.vanasrivilai@maliev.com>
2026-06-07 22:26:25 +0700

    test(client): add failing tests for ThumbnailGenerationService cache

---
+++ Maliev.Intranet.Tests/Client/Services/ThumbnailGenerationServiceTests.cs
@@ -0,0 +1,56 @@
+using Maliev.Intranet.Client.Services;
+using Maliev.Intranet.Shared.Dtos;
+using Microsoft.Extensions.Logging.Abstractions;
+using Xunit;
+
+namespace Maliev.Intranet.Tests.Client.Services;
+
+public class ThumbnailGenerationServiceTests
+{
+    [Fact]
+    public void TryGetCached_WhenCacheEmpty_ReturnsFalse()
+    {
+        var service = new ThumbnailGenerationService(
+            jsRuntime: null!,
+            httpClient: new HttpClient(),
+            logger: NullLogger<ThumbnailGenerationService>.Instance);
+
+        var result = service.TryGetCached("path/file.stl", out var set);
+
+        Assert.False(result);
+        Assert.Null(set);
+    }
+
+    [Fact]
+    public void TryGetCached_WhenCached_ReturnsTrue()
+    {
+        var service = new ThumbnailGenerationService(
+            jsRuntime: null!,
+            httpClient: new HttpClient(),
+            logger: NullLogger<ThumbnailGenerationService>.Instance);
+
+        var cachedSet = new ThumbnailSetDto { ThumbnailSmall = "data:image/jpeg;base64,abc" };
+        service.SetCached("path/file.stl", "v1", cachedSet);
+
+        var result = service.TryGetCached("path/file.stl", "v1", out var set);
+
+        Assert.True(result);
+        Assert.Same(cachedSet, set);
+    }
+
+    [Fact]
+    public void TryGetCached_WhenVersionMismatch_ReturnsFalse()
+    {
+        var service = new ThumbnailGenerationService(
+            jsRuntime: null!,
+            httpClient: new HttpClient(),
+            logger: NullLogger<ThumbnailGenerationService>.Instance);
+
+        service.SetCached("path/file.stl", "v1", new ThumbnailSetDto { ThumbnailSmall = "abc" });
+
+        var result = service.TryGetCached("path/file.stl", "v2", out var set);
+
+        Assert.False(result);
+        Assert.Null(set);
+    }
+}
