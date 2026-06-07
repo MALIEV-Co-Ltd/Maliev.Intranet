92666f5875ded003f00286a149706137671e70e8
Natthapol Vanasrivilai <natthapol.vanasrivilai@maliev.com>
2026-06-07 22:43:18 +0700

    test(client): add failing tests for subscribe/notify

--- Maliev.Intranet.Tests/Client/Services/ThumbnailGenerationServiceTests.cs
+++ Maliev.Intranet.Tests/Client/Services/ThumbnailGenerationServiceTests.cs
@@ -1,5 +1,6 @@
 using Maliev.Intranet.Client.Services;
 using Maliev.Intranet.Shared.Dtos;
+using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Logging.Abstractions;
 using Xunit;
 
@@ -52,5 +53,47 @@ public class ThumbnailGenerationServiceTests
 
         Assert.False(result);
         Assert.Null(set);
+    }
+
+    [Fact]
+    public async Task NotifyAsync_InvokesAllSubscribers()
+    {
+        var service = new ThumbnailGenerationService(
+            jsRuntime: null!,
+            httpClient: new HttpClient(),
+            logger: NullLogger<ThumbnailGenerationService>.Instance);
+
+        var received1 = new List<ThumbnailProgress>();
+        var received2 = new List<ThumbnailProgress>();
+        var callback1 = EventCallback.Factory.Create<ThumbnailProgress>(this, p => received1.Add(p));
+        var callback2 = EventCallback.Factory.Create<ThumbnailProgress>(this, p => received2.Add(p));
+
+        service.Subscribe(callback1);
+        service.Subscribe(callback2);
+
+        var progress = new ThumbnailProgress("path/file.stl", ThumbnailGenerationStage.Downloading, 10, "test");
+        await service.NotifyAsync(progress);
+
+        Assert.Single(received1);
+        Assert.Single(received2);
+        Assert.Equal("path/file.stl", received1[0].StoragePath);
+    }
+
+    [Fact]
+    public void Unsubscribe_RemovesCallback()
+    {
+        var service = new ThumbnailGenerationService(
+            jsRuntime: null!,
+            httpClient: new HttpClient(),
+            logger: NullLogger<ThumbnailGenerationService>.Instance);
+
+        var received = new List<ThumbnailProgress>();
+        var callback = EventCallback.Factory.Create<ThumbnailProgress>(this, p => received.Add(p));
+
+        service.Subscribe(callback);
+        service.Unsubscribe(callback);
+        service.NotifyAsync(new ThumbnailProgress("path", ThumbnailGenerationStage.Queued, 0, null)).Wait();
+
+        Assert.Empty(received);
     }
 }
